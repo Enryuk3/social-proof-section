05822631f6e80f37470c4a2db93864baa827c2cf
Cristopher Guzmán <guzmncristopher@gmail.com>
2025-05-10 13:12:00 -0500

    :sparkles: create hero

---
+++ src/components/hero.jsx
@@ -0,0 +1,15 @@
+export const Hero = () => {
+  return (
+    <section className="mb-10 xl:flex-1">
+
+      <h1 className="text-very-dark-magenta text-[2.5rem]/[2rem] font-bold px-8 text-center mb-4 xl:text-left xl:w-[445px] xl:text-[3.5rem]/[3rem] xl:p-0">
+         10,000+ of our users love our products.
+      </h1>
+      <p className="text-dark-grayish-magenta text-center font-medium text-[1.0938rem]/[1.5625rem] xl:w-[445px] xl:text-[1.0625rem] xl:text-left">
+        We only provide great products combined with excellent customer service.
+        See what our satisfied customers are saying about our services.
+      </p>
+
+    </section>
+  )
+}
